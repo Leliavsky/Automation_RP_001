1921ceee073326a0f7d800b79cfc2a43ded62c31
Leliavsky <lelyavskij@gmail.com>
2023-04-22 22:58:57 +0300

    added logger

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -7,10 +7,14 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.TimeUnit;
 
 public class BasePage {
+
+    protected static final Logger LOGGER = LogManager.getLogger();
     protected WebDriver driver;
 
     public BasePage(WebDriver driver) {
